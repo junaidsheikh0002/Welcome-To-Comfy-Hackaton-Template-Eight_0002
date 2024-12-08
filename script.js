882a192745c84f6cdb9b05056c9c6a999aad4af6
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2024-12-08 20:53:02 +0500

    https://github.com/junaidsheikh0002/Welcome-To-Comfy-Hackaton-Template-Eight_0002/upload
    https://github.com/junaidsheikh0002/Welcome-To-Comfy-Hackaton-Template-Eight_0002/upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const heroButton = document.querySelector("button");
+    heroButton.addEventListener("click", function() {
+        alert("Redirecting to shop...");
+        // Redirect to shop page or perform any action
+    });
+});
